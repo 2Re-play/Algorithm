6e0b8cf1b2094a90f6963cb6e7a8414219e1af5c
2Re-play <wogusduddnjs@gmail.com>
2020-03-04 17:33:41 +0900

    [solve] 프로그래머스 주식가격

--- src/programmers/P_makeMaxNumber.java
+++ src/programmers/P_makeMaxNumber.java
@@ -8,6 +8,11 @@ public class P_makeMaxNumber {
     public static String solution(String number, int k) {
         String answer = "";
 
+        for(int i=0; i<number.length(); i++) {
+            for(int j=i+1; j<number.length(); j++) {
+
+            }
+        }
 
 
         return answer;
@@ -16,6 +21,6 @@ public class P_makeMaxNumber {
 
     public static void main (String[] args) {
 
-        System.out.println(solution("1924", 2));
+        System.out.println(solution("1924", 2)); //94
     }
 }
