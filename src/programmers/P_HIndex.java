3d29ef6d93272253bf9c04455a6f281c9ac0c5e7
2Re-play <wogusduddnjs@gmail.com>
2020-03-04 18:14:06 +0900

    [solve] 프로그래머스 H-Index

--- src/programmers/P_HIndex.java
+++ src/programmers/P_HIndex.java
@@ -1,32 +1,25 @@
-//import java.util.Arrays;
-//
-//public class P_HIndex {
-//
-//    public static int solution(int[] citations) {
-//        int answer = 0;
-//        int sum = 0;
-//        int avg = 0;
-//        int max = 0;
-//        int min = 0;
-//        for(int i=0; i<citations.length; i++) {
-//            sum += citations[i];
-//        }
-//        avg = sum / citations.length;
-//        for(int j=0; j<citations.length; j++) {
-//            if(avg < citations[j]) {
-//                min++;
-//            } else {
-//                max++;
-//            }
-//        }
-//        if(min <= avg && max >= avg) {
-//        } else if (min )
-//        return answer;
-//    }
-//
-//    public static void main (String[] args) {
-//        int[] citations = {3, 0, 6, 1, 5};
-//        System.out.println(solution(citations));
-//
-//    }
-//}
+package programmers;
+
+import java.util.Arrays;
+
+public class P_HIndex {
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for(int i=0; i<citations.length; i++) {
+            if(citations.length - i <= citations[i]){
+                answer = citations.length - i;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main (String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+        System.out.println(solution(citations));
+
+    }
+}
