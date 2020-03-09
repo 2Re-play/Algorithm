ff14bd8b63b58cef4ec8614bc8901314cc8c3801
2Re-play <wogusduddnjs@gmail.com>
2020-03-10 01:49:28 +0900

    modified 예외 처리추가

--- src/baekJoon/DP/B_2156.java
+++ src/baekJoon/DP/B_2156.java
@@ -26,12 +26,17 @@ public class B_2156 {
         }
 
         dp[0] = 0;
-        dp[1] = input[1];
-        dp[2] = input[1] + input[2];
-
-
-        for(int i=3; i<=n; i++) {
-            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + input[i] , dp[i-3] + input[i-1] + input[i]));
+        if(n == 1) {
+            dp[1] = input[1];
+        } else if (n == 2) {
+            dp[1] = input[1];
+            dp[2] = input[1] + input[2];
+        } else {
+            dp[1] = input[1];
+            dp[2] = input[1] + input[2];
+            for(int i=3; i<=n; i++) {
+                dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + input[i] , dp[i-3] + input[i-1] + input[i]));
+            }
         }
 
         System.out.println(dp[n]);
