477a61f97dd5f73a458669e475b717e068741307
2Re-play <wogusduddnjs@gmail.com>
2020-03-10 18:10:15 +0900

    [solve] 백준 가장 긴 증가,감소하는 부분 수열

---
+++ src/baekJoon/DP/B_11053.java
@@ -0,0 +1,41 @@
+package baekJoon.DP;
+
+import java.util.Scanner;
+
+public class B_11053 {
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] input = new int[n+1];
+        int[] dp = new int[n+1];
+
+        for(int i=1; i<=n; i++) {
+            input[i] = sc.nextInt();
+        }
+        dp[1] = 1;
+
+        if(n>1) {
+            for(int i=2; i<=n; i++) {
+                dp[i] = 1;
+                for(int j=1; j<i; j++) {
+                    if(input[j] == input[i])
+                        dp[i] = dp[j];
+                    else if (input[i] > input[j] && dp[i] <= dp[j])
+                        dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for(int i=1; i<=n; i++) {
+            max = max < dp[i] ? dp[i] : max;
+        }
+        System.out.println(max);
+
+    }
+
+    public static void main(String [] args) {
+
+        solution();
+    }
+}
