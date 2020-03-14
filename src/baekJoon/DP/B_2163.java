9790130d68753e2ee14dfc53b2a31abbdc02c1d2
2Re-play <wogusduddnjs@gmail.com>
2020-03-14 21:10:01 +0900

    [unsolve] 백준 초콜릿 자르기(DP)

---
+++ src/baekJoon/DP/B_2163.java
@@ -0,0 +1,17 @@
+package baekJoon.DP;
+
+import java.util.Scanner;
+
+public class B_2163 {
+
+    // 초콜릿 자르기
+    public static void solution () {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+    }
+
+    public static void main (String [] args) {
+        solution();
+    }
+}
