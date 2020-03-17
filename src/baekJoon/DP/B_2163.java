5b5b993d79192b03e6e46227479f4366ee25e24a
2Re-play <wogusduddnjs@gmail.com>
2020-03-17 15:14:32 +0900

    [solve] 백준 초콜릿 자르기 (DP)

--- src/baekJoon/DP/B_2163.java
+++ src/baekJoon/DP/B_2163.java
@@ -9,6 +9,16 @@ public class B_2163 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+
+        int[] dp = new int[m+1];
+
+        dp[1] = n-1;
+
+        for(int i=2; i<=m; i++) {
+            dp[i] = dp[i-1] + n;
+        }
+
+        System.out.println(dp[m]);
     }
 
     public static void main (String [] args) {
