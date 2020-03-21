b1aefcd6bacc732775b7cf7ee692ed50b30e1908
2Re-play <wogusduddnjs@gmail.com>
2020-03-22 00:15:34 +0900

    [solve] 백준 오르막수

--- src/baekJoon/DP/B_11057.java
+++ src/baekJoon/DP/B_11057.java
@@ -1,9 +1,35 @@
 package baekJoon.DP;
 
+import java.util.Scanner;
+
 public class B_11057 {
 
     // 오르막 수
     public static void solution () {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[][] dp = new long[n+1][11];
+        long result = 0;
+
+        for(int i=1; i<=10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for(int i=2; i<=n; i++) {
+            for(int j=1; j<=10; j++) {
+                for(int k=1; k<=j; k++) {
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+
+        for(int k=1; k<=10; k++) {
+            result += dp[n][k] % 10007 ;
+        }
+
+        System.out.println(result % 10007);
 
     }
 
