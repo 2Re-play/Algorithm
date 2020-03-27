9322dd6a0c89b239f4e1947b8bb9e663201914cd
2Re-play <wogusduddnjs@gmail.com>
2020-03-27 16:53:20 +0900

    [unsolve] 백준 로또

---
+++ src/baekJoon/BurteForce/B_6603.java
@@ -0,0 +1,14 @@
+package baekJoon.BurteForce;
+
+public class B_6603 {
+
+    // 로
+    public static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
+또
