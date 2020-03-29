122dc1beaf6287d347fda3410b88f0725656d9e7
2Re-play <wogusduddnjs@gmail.com>
2020-03-29 22:22:34 +0900

    [unsolve] 백준 로또

--- src/baekJoon/BurteForce/B_6603.java
+++ src/baekJoon/BurteForce/B_6603.java
@@ -1,10 +1,25 @@
 package baekJoon.BurteForce;
 
+import java.util.Scanner;
+
 public class B_6603 {
 
     // 로또
     public static void solution () {
 
+        Scanner sc = new Scanner(System.in) ;
+
+        while (true) {
+            int T = sc.nextInt();
+            if(T == 0) break;
+            int[] input = new int[T];
+
+            for(int i=0; i<T; i++) {
+                input[i] = sc.nextInt();
+            }
+
+
+        }
     }
 
     public static void main(String[] args) {
