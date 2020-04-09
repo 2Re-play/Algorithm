32a1c75a03a13d0fb77da7d58fdddecd89546c5f
2Re-play <wogusduddnjs@gmail.com>
2020-04-09 21:54:05 +0900

    [unsolve] 백준 동전2

---
+++ src/baekJoon/DP/B_2294.java
@@ -0,0 +1,16 @@
+package baekJoon.DP;
+
+import java.util.Scanner;
+
+// 동전2
+public class B_2294 {
+
+    private static void solution () {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
