41caa1e9a99a151728643248104a0b5c8c5207a9
2Re-play <wogusduddnjs@gmail.com>
2020-04-10 20:54:06 +0900

    [unsolve] 백준 덩치

---
+++ src/baekJoon/BurteForce/B_7568.java
@@ -0,0 +1,15 @@
+package baekJoon.BurteForce;
+
+import java.util.Scanner;
+
+public class B_7568 {
+
+    private static void solution () {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
