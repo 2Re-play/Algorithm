a7a39b20058dd0731fd9f08a43912fe9dccd1674
2Re-play <wogusduddnjs@gmail.com>
2020-04-12 15:23:04 +0900

    [solved] 백준 동전2

--- src/baekJoon/DP/B_2294.java
+++ src/baekJoon/DP/B_2294.java
@@ -8,6 +8,28 @@ public class B_2294 {
     private static void solution () {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] input = new int[n+1];
+        int[] dp = new int[100001];
+
+        for(int i=1; i<=n; i++) {
+            input[i] = sc.nextInt();
+        }
+        for(int j=1; j<=k; j++) {
+            dp[j] = 100001; // 최솟값을 문제기 때문에 최댓값 + 1로 초기 세팅
+        }
+        dp[0] = 0;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=input[i]; j<=k; j++) {
+                dp[j] = Math.min(dp[j], dp[j-input[i]] + 1);
+            }
+        }
+
+        System.out.println(dp[k] == 100001 ? -1 : dp[k]);
+
     }
 
     public static void main(String[] args) {
