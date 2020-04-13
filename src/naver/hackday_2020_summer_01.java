f8758b72c80621d56907217d46b0586066f7bb78
2Re-play <wogusduddnjs@gmail.com>
2020-04-13 23:11:35 +0900

    [solve] 2020 summer 네이버 핵데이 1번

--- src/naver/hackday_2020_summer_01.java
+++ src/naver/hackday_2020_summer_01.java
@@ -12,37 +12,37 @@ public class hackday_2020_summer_01 {
         // 없으면 X 있으면 O 모르면 ?
         // 배송완료 1, 미배송 0
 
-        for(int i=0; i<delivery.length; i++) {
-            if(delivery[i][2] == 1) { // 배송 완료
-                if(!remindProduct.contains(delivery[i][0])) { // 배송 완료인데 데이터가 없는 경우 넣기
-                    remindProduct.add(delivery[i][0]);
-                    sb.append("O");
+        for(int j=0; j<2; j++) {
+            for(int i=0; i<delivery.length; i++) {
+                if(delivery[i][2] == 1) { // 배송 완료
+                    if(!remindProduct.contains(delivery[i][0])) { // 배송 완료인데 데이터가 없는 경우 넣기
+                        remindProduct.add(delivery[i][0]);
+                    }
+                    if(!remindProduct.contains(delivery[i][1])) { // 배송 완료인데 데이터가 없는 경우 넣기
+                        remindProduct.add(delivery[i][1]);
+                    }
                 }
-                if(!remindProduct.contains(delivery[i][1])) { // 배송 완료인데 데이터가 없는 경우 넣기
-                    remindProduct.add(delivery[i][1]);
-                    sb.append("O");
-                }
-            }
-            if(delivery[i][2] == 0) { // 미배송
-                if(remindProduct.contains(delivery[i][0]) && !remindProduct.contains(delivery[i][1]) && !noRemindProduct.contains(delivery[i][1])) { // 첫번째 제품은 재고 있고 두번째는 없는 경우
-                    sb.append("X");
-                    noRemindProduct.add(delivery[i][1]);
-                } else if (remindProduct.contains(delivery[i][1]) && !remindProduct.contains(delivery[i][0]) && !noRemindProduct.contains(delivery[i][0])) {
-                    sb.append("X");
-                    noRemindProduct.contains(delivery[i][0]);
-                } else if (noRemindProduct.contains(delivery[i][0]) && !remindProduct.contains(delivery[i][1]) && !noRemindProduct.contains(delivery[i][1])) {
-                    sb.append("?");
-                } else if (noRemindProduct.contains(delivery[i][1]) && !remindProduct.contains(delivery[i][0]) && !noRemindProduct.contains(delivery[i][0])) {
-                    sb.append("?");
+                if(delivery[i][2] == 0) { // 미배송
+                    if(remindProduct.contains(delivery[i][0]) && !remindProduct.contains(delivery[i][1]) && !noRemindProduct.contains(delivery[i][1])) { // 첫번째 제품은 재고 있고 두번째는 없는 경우
+                        noRemindProduct.add(delivery[i][1]);
+                    } else if (remindProduct.contains(delivery[i][1]) && !remindProduct.contains(delivery[i][0]) && !noRemindProduct.contains(delivery[i][0])) {
+                        noRemindProduct.contains(delivery[i][0]);
+                    }
                 }
             }
+        }
 
+        for(int i=1; i<=n; i++) {
+            if(remindProduct.contains(i)) {
+                sb.append("O");
+            } else if (noRemindProduct.contains(i)) {
+                sb.append("X");
+            } else {
+                sb.append("?");
+            }
         }
 
         System.out.println(sb.toString());
-
-
-
     }
 
     public static void main(String[] args) {
