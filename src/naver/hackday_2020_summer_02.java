0929e18b1174f537ae91dbd16c43c17de54695d7
2Re-play <wogusduddnjs@gmail.com>
2020-04-14 16:55:07 +0900

    [unsolve] 2020 summer 네이버 핵데이 2번

---
+++ src/naver/hackday_2020_summer_02.java
@@ -0,0 +1,18 @@
+package naver;
+
+public class hackday_2020_summer_02 {
+
+    private static void solution(String[] id_list, int k) {
+
+    }
+
+    public static void main(String[] args) {
+        String[] id_list1 = {"A B C D", "A D", "A B D", "B D"};
+        String[] id_list2 = {"JAY", "JAY ELLE JAY MAY", "MAY ELLE MAY","ELLE MAY", "ELLE ELLE ELLE", "MAY"};
+        int k1 = 2; // result : 7
+        int k2 = 3; // result : 8
+
+        solution(id_list1, k1);
+
+    }
+}
