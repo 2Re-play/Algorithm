d73bd676f59791f5b5dace5519200cffd2db6d8a
2Re-play <wogusduddnjs@gmail.com>
2020-04-15 11:44:13 +0900

    [unsolve] 2020 summer 네이버 핵데이 3번

---
+++ src/naver/hackday_2020_summer_03.java
@@ -0,0 +1,33 @@
+package naver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class hackday_2020_summer_03 {
+
+    private static void solution(int n, int m, int k) {
+
+    }
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 10;
+        int n3 = 2;
+        int n4 = 50;
+
+        int m1 = 8;
+        int m2 = 6;
+        int m3 = 10;
+        int m4 = 150;
+
+        int k1 = 4; // 6
+        int k2 = 5; // 0
+        int k3 = 4; // 0
+        int k4 = 20; //780361386
+
+        solution(n1, m1, k1);
+
+    }
+}
