f044d39f667256f4d2d55964a359dd1458293971
2Re-play <wogusduddnjs@gmail.com>
2020-04-22 23:38:15 +0900

    [unsolved] 백준 부분수열의 합

---
+++ src/baekJoon/BurteForce/b_1182.java
@@ -0,0 +1,21 @@
+package baekJoon.BurteForce;
+
+import java.util.Scanner;
+
+public class b_1182 {
+
+    private static void solution () {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] input = new int[n];
+        for(int i=0;i<n; i++) {
+            input[i] = sc.nextInt();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
