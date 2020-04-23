adaf240bcb7c790be5445d823a6bbad10f2d5a40
2Re-play <wogusduddnjs@gmail.com>
2020-04-23 23:34:06 +0900

    [unsolved] 백준 이항계수 2

---
+++ src/baekJoon/DP/B_11051.java
@@ -0,0 +1,14 @@
+package baekJoon.DP;
+
+public class B_11051 {
+
+    // 이항 계수 2
+    private static void solutiuon () {
+
+
+    }
+
+    public static void main(String[] args) {
+        solutiuon();
+    }
+}
