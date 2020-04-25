41f7145d137203bf7e91c5bf6f91c9c73c406e21
2Re-play <wogusduddnjs@gmail.com>
2020-04-25 17:30:36 +0900

    [solve] 백준 프린터 큐

--- src/baekJoon/BurteForce/B_1966.java
+++ src/baekJoon/BurteForce/B_1966.java
@@ -16,49 +16,45 @@ public class B_1966 {
     private static void solution () {
         Scanner sc = new Scanner(System.in);
         int testCase = sc.nextInt();
-        Queue<Document> queue = new LinkedList<Document>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
+        int[] result = new int[testCase];
         for(int i=0; i<testCase; i++) {
-            queue.clear();
             int n = sc.nextInt();
-            int target = sc.nextInt();
+            int targetIndex = sc.nextInt();
+            LinkedList<Document> queue = new LinkedList<Document>();
+
             for(int j=0; j<n; j++) {
-                queue.add(new Document(j, sc.nextInt()));
+                queue.offer(new Document(j, sc.nextInt()));
             }
+
             int count = 1;
-            while (!queue.isEmpty()) {
-                int max = getMax(list);
-                Document temp = queue.poll();
-                if(temp.priority == max) {
-                    if(temp.index == target) {
-                        System.out.println(count);
+            while(true) {
+                if(isMax(queue)) {
+                    if(queue.get(0).index == targetIndex) {
+                        result[i] = count;
                         break;
+                    } else {
+                        queue.poll();
+                        count++;
                     }
-                    count++;
-                    list.remove(list.indexOf(max));
-                    continue;
+                } else {
+                    queue.offer(queue.poll());
                 }
-                queue.offer(temp);
             }
         }
+
+        for(int item : result) {
+            System.out.println(item);
+        }
+
     }
 
-    private static int getMax(ArrayList<Integer> list) {
-        int max = Integer.MIN_VALUE;
-        for (int i=0; i<list.size(); i++) {
-            max = max < list.get(i) ? list.get(i) : max;
+    private static boolean isMax (LinkedList<Document> queue) {
+        for(int i=1; i<queue.size(); i++) {
+            if(queue.getFirst().priority < queue.get(i).priority) {
+                return false;
+            }
         }
-        return max;
-    }
-
-    private static int findIndex (ArrayList<Integer> list, int priority) {
-        int index = 0;
-        for (int i=0; i<list.size(); i++) {
-            index++;
-            if(list.get(i) > priority) break;
-        }
-        return (index == list.size() ? 0 : index);
+        return true;
     }
 
     public static void main(String[] args) {
