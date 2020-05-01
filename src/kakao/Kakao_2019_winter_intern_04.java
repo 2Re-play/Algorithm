330d2c2df242432b1e57922eb388a57c6aaf4dbb
2Re-play <wogusduddnjs@gmail.com>
2020-05-01 21:00:25 +0900

    [unsolve] 2019 카카오 winter internship 4번 문제

---
+++ src/kakao/Kakao_2019_winter_intern_04.java
@@ -0,0 +1,20 @@
+package kakao;
+
+
+public class Kakao_2019_winter_intern_04 {
+
+    private static long[] solution(long k, long[] room_number) {
+        long[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        long k = 10;
+        long[] room_number = {1,3,4,1,3,1};
+        System.out.println(solution(k, room_number));
+
+    }
+}
