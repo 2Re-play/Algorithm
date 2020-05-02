bc83bb720cc9f40ab04463e8e786ce90d8d3e106
2Re-play <wogusduddnjs@gmail.com>
2020-05-02 16:01:21 +0900

    [solve + 효율성 테스트 미통과] 2019 카카오 winter internship 4번 문제

--- src/kakao/Kakao_2019_winter_intern_04.java
+++ src/kakao/Kakao_2019_winter_intern_04.java
@@ -1,12 +1,31 @@
 package kakao;
 
+import java.util.ArrayList;
 
 public class Kakao_2019_winter_intern_04 {
 
     private static long[] solution(long k, long[] room_number) {
-        long[] answer = {};
+        long[] answer = new long[room_number.length];
+        ArrayList<Long> list = new ArrayList<Long>();
 
-
+        for(int i=0; i<room_number.length; i++) {
+            long room = room_number[i];
+            if(list.contains(room)) { // 만약 room이 이미 예약된 경우
+                long index = room + 1;
+                while (true) {
+                    if(list.contains(index)) {
+                        index++;
+                    } else {
+                        list.add(index);
+                        answer[i] = index;
+                        break;
+                    }
+                }
+            } else {
+                list.add(room);
+                answer[i] = room;
+            }
+        }
         return answer;
     }
 
