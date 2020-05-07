a2a04811ec4de21718a779cad1163452d7495808
2Re-play <wogusduddnjs@gmail.com>
2020-05-07 23:21:46 +0900

    [unsolve]2020 kakao blind 02

---
+++ src/kakao/Kakao_2020_blind_02.java
@@ -0,0 +1,21 @@
+package kakao;
+
+public class Kakao_2020_blind_02 {
+
+
+    public static String solution(String p) {
+        String answer = "";
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        String p1 = "(()())()"; // 7
+        String p2 = ")("; // 9
+        String p3 = "()))((()"; // 8
+        System.out.println(solution(p1));
+
+    }
+}
