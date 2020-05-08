3bec0c30e80ed1739b898cdff220155aa0e8c8c6
2Re-play <wogusduddnjs@gmail.com>
2020-05-08 20:22:10 +0900

    [unsolve]2020 kakao blind 03

---
+++ src/kakao/Kakao_2020_blind_03.java
@@ -0,0 +1,21 @@
+package kakao;
+
+public class Kakao_2020_blind_03 {
+
+
+    public static boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] key = {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}};
+        int[][] lock = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        System.out.println(solution(key, lock));
+
+    }
+}
