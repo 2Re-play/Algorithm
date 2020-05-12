e71d73e9e152c14d4084949b0866f1b4e7835ff0
2Re-play <wogusduddnjs@gmail.com>
2020-05-12 22:37:37 +0900

    [unsolved] 백준 단지번호붙이기

---
+++ src/baekJoon/BFS/B_2667.java
@@ -0,0 +1,17 @@
+package baekJoon.BFS;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class B_2667 {
+
+    // 단지번호붙이기
+    public static void solution () {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
