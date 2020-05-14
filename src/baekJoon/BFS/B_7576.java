8ac2394274f79071f1c05209d2e5178d90e421bf
2Re-play <wogusduddnjs@gmail.com>
2020-05-14 23:34:31 +0900

    [unsolved] 백준 7576 토마토

---
+++ src/baekJoon/BFS/B_7576.java
@@ -0,0 +1,16 @@
+package baekJoon.BFS;
+
+import java.util.Scanner;
+
+public class B_7576 {
+
+    // 토마토
+    private static void solution () {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
