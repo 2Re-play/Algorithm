b41da710e94e471b226acde3693aee9531d879c6
2Re-play <wogusduddnjs@gmail.com>
2020-05-21 17:44:55 +0900

    [unsolved] 백준 1012 유기농 배추

---
+++ src/baekJoon/BFS/B_1012.java
@@ -0,0 +1,12 @@
+package baekJoon.BFS;
+
+public class B_1012 {
+
+    // 유기농 배추
+    private static void solution () {
+
+    }
+    public static void main(String[] args) {
+        solution();
+    }
+}
