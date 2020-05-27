6e3e528be60dabf8cc806e432cb27fd60b06323e
2Re-play <wogusduddnjs@gmail.com>
2020-05-27 20:38:00 +0900

    [unsolved] 백준 연구소 dfs,bfs 활용

---
+++ src/baekJoon/BFS/B_14052.java
@@ -0,0 +1,121 @@
+package baekJoon.BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class B_14052 {
+
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+    static int N;
+    static int M;
+    static int[][] map;
+    static int[][] tempMap;
+    static boolean[][] visited;
+    static int answer = 0;
+
+    public static void solution () {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        map = new int[N][M];
+        tempMap = new int[N][M];
+        visited = new boolean[N][M];
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                tempMap[i][j] = map[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(tempMap[i][j] == 0) {
+                    tempMap[i][j] = 1; // 벽 하나 세우기
+                    dfs(1);
+                    tempMap[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static void dfs (int count) {
+        if(count == 3) {
+            bfs(); // 바이러스 확산 시키기
+            return;
+        }
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(tempMap[i][j] == 0) {
+                    tempMap[i][j] = 1; // 벽 2개 더 세우기
+                    dfs(count + 1);
+                    tempMap[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void bfs () {
+        Queue<int[]> queue = new LinkedList<int[]>();
+        int[][] virusMap = new int[N][M];
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                virusMap[i][j] = tempMap[i][j];
+            }
+        }
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(virusMap[i][j] == 2) {
+                    queue.add(new int[] {i, j});
+                }
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int[] data = queue.poll();
+            int curX = data[0];
+            int curY = data[1];
+            visited[curX][curY] = true;
+
+            for(int i=0; i<4; i++) {
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
+
+                if(nextX >= 0 && nextY >= 0 && nextX < N && nextY < M ) {
+                    if(virusMap[nextX][nextY] == 0 && !visited[nextX][nextY]) {
+                        virusMap[nextX][nextY] = 2;
+                        queue.add(new int[] {nextX, nextY});
+                    }
+                }
+            }
+        }
+        calAnswer(virusMap);
+    }
+
+    public static void calAnswer(int[][] virusMap) {
+        int cnt = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++){
+//                System.out.print(tempMap[i][j]);
+                if(virusMap[i][j] == 0) cnt ++;
+            }
+//            System.out.println();
+        }
+//        System.out.println();
+
+
+
+        answer = Math.max(cnt, answer);
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
