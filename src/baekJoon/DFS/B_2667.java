7e549c213ebddfe540d32e13969aa0527663b0d8
2Re-play <wogusduddnjs@gmail.com>
2020-05-28 18:53:05 +0900

    [unsolved] 백준 단지 번호 붙이기 - dfs

---
+++ src/baekJoon/DFS/B_2667.java
@@ -0,0 +1,13 @@
+package baekJoon.DFS;
+
+public class B_2667 {
+
+    // 단지 번호 붙이 - dfs
+    public static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
