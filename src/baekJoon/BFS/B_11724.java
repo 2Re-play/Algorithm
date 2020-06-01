d30f81ac8c2bf877ffe69830a46e97947df1de91
2Re-play <wogusduddnjs@gmail.com>
2020-06-02 00:36:23 +0900

    [solved] 백준 연결요소

--- src/baekJoon/BFS/B_11724.java
+++ src/baekJoon/BFS/B_11724.java
@@ -1,39 +1,63 @@
 package baekJoon.BFS;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 // 연결요소
 public class B_11724 {
-
+    static ArrayList<ArrayList<Integer>> list;
+    static boolean[] visited;
     private static void solution () {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-        int[][] map = new int[M][2];
-        for(int i=0; i<M; i++) {
-            map[i][0] = sc.nextInt();
-            map[i][1] = sc.nextInt();
+
+        visited = new boolean[N+1];
+        list = new ArrayList<ArrayList<Integer>>();
+
+        for(int i=0; i<N+1; i++) {
+            list.add(new ArrayList<Integer>());
         }
 
+        for(int i=0; i<M; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
 
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+        int count = 0;
+        for(int i=1; i<N+1; i++) {
+            if(!visited[i]) {
+                bfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
-    private static int bfs (int M, int[][] map) {
-        boolean[] visited = new boolean[M];
-        Queue<int[]> queue = new LinkedList<int[]>();
 
-        queue.add(new int[] {map[1][0], map[1][1]});
+    private static void bfs (int n) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(n);
 
         while(!queue.isEmpty()) {
-            int[] data = queue.poll();
-        }
+            int data = queue.poll();
+            visited[data] = true;
 
-        return 0;
+            for(int i=0; i< list.get(data).size(); i++) {
+                int result = list.get(data).get(i);
+                if(!visited[result]) {
+                    visited[result] = true;
+                    queue.add(result);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        solution();
     }
 }
