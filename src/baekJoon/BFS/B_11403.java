3b3edec7b27bc6d40fe08745ed8e5eb78ce46d8f
2Re-play <wogusduddnjs@gmail.com>
2020-06-04 18:59:12 +0900

    [solved] 백준 경로 찾기

--- src/baekJoon/BFS/B_11403.java
+++ src/baekJoon/BFS/B_11403.java
@@ -1,10 +1,60 @@
 package baekJoon.BFS;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class B_11403 {
 
     // 경로 찾기
+    static int[][] map;
+    static int n;
+    static boolean[]visited;
     private static void solution () {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        map = new int[n][n];
 
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            visited = new boolean[n];
+            for(int j=0; j<n; j++) {
+                if(!visited[j] && map[i][j] == 1) {
+                    bfs(i, j);
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(y);
+
+        while (!queue.isEmpty()) {
+            int data = queue.poll();
+
+            for(int i=0; i<n; i++) {
+                if(!visited[i] && map[data][i] == 1) {
+                    queue.add(i);
+                    map[x][i] = 1;
+                    visited[i] = true;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
