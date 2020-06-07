d5e27a6508aa5201349c5ef471a49090c6a2fa62
2Re-play <wogusduddnjs@gmail.com>
2020-06-07 17:48:06 +0900

    [init] 백준 안전 영역

---
+++ src/baekJoon/BFS/B_2468.java
@@ -0,0 +1,16 @@
+package baekJoon.BFS;
+
+import java.util.Scanner;
+
+public class B_2468 {
+
+    // 안정 영역
+    private static void solution () {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
