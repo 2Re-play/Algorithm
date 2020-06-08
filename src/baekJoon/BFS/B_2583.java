063573ed51e1b7a9e3d64bc186300d56f9c71ffa
2Re-play <wogusduddnjs@gmail.com>
2020-06-08 18:42:16 +0900

    [init] 백준 영역구하기

---
+++ src/baekJoon/BFS/B_2583.java
@@ -0,0 +1,13 @@
+package baekJoon.BFS;
+
+public class B_2583 {
+
+    // 영역 구하기
+    private static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
