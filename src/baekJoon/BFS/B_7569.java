d971ff586beb4984f3c4d9b301621402d89997fc
2Re-play <wogusduddnjs@gmail.com>
2020-06-12 18:55:43 +0900

    [unsolved] 백준 토마토2

--- src/baekJoon/BFS/B_7569.java
+++ src/baekJoon/BFS/B_7569.java
@@ -1,9 +1,17 @@
 package baekJoon.BFS;
 
+import java.util.Scanner;
+
 public class B_7569 {
 
     // 토마토
     private static void solution () {
+
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int h = sc.nextInt();
+
 
     }
 
