c54a0fdcd7434bc34ae06b5221dfc331898019c5
2Re-play <wogusduddnjs@gmail.com>
2020-06-14 17:58:23 +0900

    [init] 백준 달팽이는 올라가고 싶다.

---
+++ src/baekJoon/BinarySearch/B_2869.java
@@ -0,0 +1,13 @@
+package baekJoon.BinarySearch;
+
+public class B_2869 {
+
+    // 달팽이는 올라가고 싶다.
+    private static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
