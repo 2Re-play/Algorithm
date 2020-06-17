94b84f0df4c33ed492f7df1708a8e33cd658ab89
2Re-play <wogusduddnjs@gmail.com>
2020-06-17 19:02:44 +0900

    [init] 백준 리모컨

---
+++ src/baekJoon/BurteForce/B_1107.java
@@ -0,0 +1,13 @@
+package baekJoon.BurteForce;
+
+public class B_1107 {
+
+    // 리모컨
+    private static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
