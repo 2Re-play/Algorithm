eb8b6bcf3e6c795a3eef9f60e7ee7cc4bedf855e
2Re-play <wogusduddnjs@gmail.com>
2020-06-18 18:22:56 +0900

    [init] 백준 10972

---
+++ src/baekJoon/Permutation/B_10972.java
@@ -0,0 +1,12 @@
+package baekJoon.Permutation;
+
+public class B_10972 {
+
+    private static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
