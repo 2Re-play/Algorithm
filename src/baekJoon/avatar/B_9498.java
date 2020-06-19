7e2b11dacdcaf5edf0b9d01b891a531e9c03a0b8
2Re-play <wogusduddnjs@gmail.com>
2020-06-19 17:26:33 +0900

    [init] 백준 시험성적

---
+++ src/baekJoon/avatar/B_9498.java
@@ -0,0 +1,13 @@
+package baekJoon.avatar;
+
+public class B_9498 {
+
+    // 시험 성적
+    private static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
