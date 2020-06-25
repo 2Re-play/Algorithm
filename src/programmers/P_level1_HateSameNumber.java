35c5ab633c47117e1a652ffbd93952fa4635d737
2Re-play <wogusduddnjs@gmail.com>
2020-06-25 19:01:01 +0900

    [init] 프로그래머스 같은 숫자는 싫어

---
+++ src/programmers/P_level1_HateSameNumber.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class P_level1_HateSameNumber {
+
+    // 프로그래머스 level1 같은 숫자는 싫어
+    private static void solution () {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
