33810b54c19412165a293c74736bf127c63bb1cd
2Re-play <wogusduddnjs@gmail.com>
2020-06-26 10:29:40 +0900

    [solved] 프로그래머스 같은 숫자는 싫어

--- src/programmers/P_level1_HateSameNumber.java
+++ src/programmers/P_level1_HateSameNumber.java
@@ -1,13 +1,34 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+
 public class P_level1_HateSameNumber {
 
     // 프로그래머스 level1 같은 숫자는 싫어
-    private static void solution () {
+    private static int[] solution (int[] arr) {
 
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        int prev = -1;
+        for(int item : arr) {
+            if(prev != item) {
+                list.add(item);
+                prev = item;
+            }
+        }
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        int[] arr1 = {1,1,3,3,0,1,1}; // [1,3,0,1]
+        int[] arr2 = {4,4,4,3,3}; // [4,3]
+        int[] anwer = solution(arr1);
+        for(int item : anwer) {
+            System.out.println(item);
+        }
     }
 }
