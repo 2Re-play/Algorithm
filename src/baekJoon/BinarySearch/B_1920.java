77f251016e9ea8ae124e0152aca79b5d86df7787
2Re-play <wogusduddnjs@gmail.com>
2020-07-01 10:38:03 +0900

    [solved] 백준 1920 수찾기

--- src/baekJoon/BinarySearch/B_1920.java
+++ src/baekJoon/BinarySearch/B_1920.java
@@ -1,9 +1,7 @@
 package baekJoon.BinarySearch;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class B_1920 {
 
@@ -13,47 +11,46 @@ public class B_1920 {
 5
 1 3 7 9 5
  */
-    public static void main (String [] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String [] beforeArray = br.readLine().split(" ");
-        int [] array = new int[n];
-
-        for (int i=0; i<n; i++) {
-            array[i] = Integer.parseInt(beforeArray[i]);
+    private static void solution () {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] array = new long[n];
+        for(int i=0; i<n; i++) {
+            array[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        long[] compareArray = new long[m];
+        for(int i=0; i<m; i++) {
+            compareArray[i] = sc.nextInt();
         }
 
-        Arrays.sort(array); // 비교대상
+        Arrays.sort(array);
 
-        int m = Integer.parseInt(br.readLine());
-        String [] beforeComapreArray = br.readLine().split(" ");
-        int [] compareArray = new int[m];
-
-        for (int j=0; j<m; j++) {
-            compareArray[j] = Integer.parseInt(beforeComapreArray[j]);
-        }
-
-        for(int k=0; k<m; k++) {
-            int flag = 0;
-            int max = m;
-            int min = -1;
+        for(int j=0; j<compareArray.length; j++) {
+            int left = -1;
+            int right = n;
             int middle;
+            int flag = 0;
+            while(right - left > 1) {
+                middle = (left + right) / 2;
 
-            while (max - min > 1) {
-                middle = (min + max) / 2;
-                if(compareArray[k] == array[middle]) {
+                if(compareArray[j] == array[middle]) {
                     flag = 1;
                     break;
-                }
-                if(compareArray[k] > array[middle]) {
-                    min = middle;
-                } else if (compareArray[k] < array[middle]) {
-                    max = middle;
+                } else {
+                    if(compareArray[j] < array[middle]) {
+                        right = middle;
+                    } else {
+                        left = middle;
+                    }
                 }
             }
             System.out.println(flag);
         }
+    }
+
+    public static void main (String [] args) {
+        solution();
     }
 
 }
